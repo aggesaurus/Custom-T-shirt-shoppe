bccb57e5e60afdc1249469a6902d8cd1ca7f6ace
aggesaurus <august.toresson@outlook.com>
2019-10-03 15:59:53 +0200

    Added new component and spec

---
+++ src/components/Preview.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+  data(){
+    return{
+      previewData:{
+        Size: '',
+        Color: '',
+        Material: '',
+        Model: '',
+        Picture: '',
+        Text: {
+          Fontstyle: '',
+          Fontsize: '',
+          FontColor: ''
+        }
+      },
+    // //   setSize(Size){
+    // //     this.fillingCustomData.Size = Size;
+    // //   },
+    // //   setColor(Color){
+    // //     this.fillingCustomData.Color = Color;
+    // //   },
+    // //   addText(Text){
+    // //     this.fillingCustomData.Text = Text;
+    //   }
+    }
+  }
+}
+</script>
