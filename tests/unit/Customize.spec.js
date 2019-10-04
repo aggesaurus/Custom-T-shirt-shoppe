94fe9134fb00a57eea140c437730047f0f3d9ec9
aggesaurus <august.toresson@outlook.com>
2019-10-04 15:11:52 +0200

    Added userstory 18 & 19 test

--- tests/unit/Customize.spec.js
+++ tests/unit/Customize.spec.js
@@ -1,16 +1,42 @@
 import { mount } from '@vue/test-utils'
 import TShirtCustomize from '@/components/TShirtCustomize.vue'
 // let customize = expectedCustomData 
+// let expectedCustomData = {
+//   Size: "Medium",
+//   Color: "Blue",
+//   Material: "Cotton",
+//   Model: "Long arms",
+//   Picture: "mycar.jpg",
+//   Text: {
+//     Fontstyle: "Calibri",
+//     Fontsize: "18",
+//     FontColor: "Yellow"
+//   }
+// }
 let expectedCustomData = {
-  Size: "Medium",
-  Color: "Blue",
-  Material: "Cotton",
-  Model: "Long arms",
-  Picture: "mycar.jpg",
-  Text: {
-    Fontstyle: "Calibri",
-    Fontsize: "18",
-    FontColor: "Yellow"
+  FrontCustomize: {
+    Size: "Medium",
+    Color: "Blue",
+    Material: "Cotton",
+    Model: "Long arms",
+    Picture: "mycar.jpg",
+    Text: {
+      Fontstyle: "Calibri",
+      Fontsize: "18",
+      FontColor: "Yellow"
+    }
+  },
+  BackCusztomize: {
+    Size: "Medium",
+    Color: "Blue",
+    Material: "Cotton",
+    Model: "Long arms",
+    Picture: "mycar.jpg",
+    Text: {
+      Fontstyle: "Calibri",
+      Fontsize: "18",
+      FontColor: "Yellow"
+    }
   }
 }
 // userstory 2: Som kund vill jag kunna välja storlek på min t-shirt. 
@@ -22,13 +48,13 @@ describe('TShirtCustomize.vue', () => {
   test('setSize', () => {
     const wrapper = mount(TShirtCustomize)
     wrapper.vm.setSize(expectedCustomData.Size)
-    expect(wrapper.vm.fillingCustomData.Size).toEqual(expectedCustomData.Size) 
+    expect(wrapper.vm.fillingCustomData.Size).toEqual(expectedCustomData.Size)
   })
   // userstory 3: Som kund vill jag kunna välja färg på min t-shirt.
   test('setColor', () => {
     const wrapper = mount(TShirtCustomize)
     wrapper.vm.setColor(expectedCustomData.Color)
-    expect(wrapper.vm.fillingCustomData.Color).toEqual(expectedCustomData.Color) 
+    expect(wrapper.vm.fillingCustomData.Color).toEqual(expectedCustomData.Color)
   })
   // userstory 4: Som kund vill jag kunna skapa min egen t-shirt.
   test('Customize my own shirt', () => {
@@ -39,6 +65,25 @@ describe('TShirtCustomize.vue', () => {
   test('Add text', () => {
     const wrapper = mount(TShirtCustomize)
     wrapper.vm.addText(expectedCustomData.Text)
-    expect(wrapper.vm.fillingCustomData.Text).toEqual(expectedCustomData.Text) 
+    expect(wrapper.vm.fillingCustomData.Text).toEqual(expectedCustomData.Text)
   })
+  //userstory 18: Som kund vill jag kunna skapa fram/baksida på min t-shirt.
+  test('Customize front', () => {
+    const wrapper = mount(TShirtCustomize)
+    expect(wrapper.vm.fillingCustomData).toBeDefined()
+  })
+  test('Customize back', () => {
+    const wrapper = mount(TShirtCustomize)
+    expect(wrapper.vm.fillingCustomData).toBeDefined()
+  })
+  //userstory 19: Som kund vill jag kunna ha samma möjligheter att arbeta med text, färg och bild på baksidan som jag har på framsidan.
+  test('Back customize for text, color & picture', () => {
+    const wrapper = mount(TShirtCustomize)
+    wrapper.vm.addText(expectedCustomData.Text)
+    wrapper.vm.setColor(expectedCustomData.Color)
+    wrapper.vm.addPicture(expectedCustomData.Picture)
+    expect(wrapper.vm.fillingCustomData.Text).toEqual(expectedCustomData.Text)
+    expect(wrapper.vm.fillingCustomData.Color).toEqual(expectedCustomData.Color)
+    expect(wrapper.vm.fillingCustomData.Picture).toEqual(expectedCustomData.Picture)
+  })  
 })
