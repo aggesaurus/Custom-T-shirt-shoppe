dff29671f292291c79ccdbb62e7b231a3ea4b7a1
sadecvietnam <alex71hang@yahoo.se>
2019-10-10 10:50:42 +0200

    Rest-server & components

---
+++ src/views/TShirt.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <TShirtCustomize class="col-6"></TShirtCustomize>
+    <Preview class="col-6"></Preview>
+  </div>
+</template>
+<script>
+  import TShirtCustomize from '@/components/TShirtCustomize.vue'
+  import Preview from '@/components/Preview.vue'
+  export default {
+    components:{
+      TShirtCustomize,
+      Preview
+    }
+  }
+</script>
