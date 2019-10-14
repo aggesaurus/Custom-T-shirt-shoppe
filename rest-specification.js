38a89505ee643142a8187765cd5ccbeda0ac0ebb
Puchasay <puchastephanie@gmail.com>
2019-10-14 11:51:10 +0200

    Some of the specs update

--- rest-specification.js
+++ rest-specification.js
@@ -1,256 +1,252 @@
 let api = [
     {
-      description: 'As a user I want to be able to add text to my t-shirt', //US5
+      description: 'Get all customize font', //US5
       request:{
         method: 'get',
         url: 'localhost:3000/rest/customize/:id',
         params: ['id']
       },
       response:{
-        content:{
+        content:[
+        {
           id: Number,
-          font_theme: Char,
-          font_color: Char,
-          font_size: Char,
-          font_style: Char
+          font_theme: String,
+          font_color: String,
+          font_size: Number,
+          font_style: String
         }
+      ]
       }
     },
     {
-      description: 'As a user I want to be able to order my t-shirt', // US8
+      description: 'Get all order information', // US8
       request:{
         method: 'get',
         url: 'localhost:3000/rest/order'
-        //params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          order_number: Vachar,
-          price: Integer,
-          amount: Interger,
-          dis_code: Vachar
+          order_number: Number,
+          price: Number,
+          amount: Number,
+          dis_code: String
           }
         ]
       }
     },
     {
-      description: 'As a user I want to be able to get a confirmation of my order', //US10
+      description: 'Create order confirmation information',  //US10
+      request:{
+        method: 'post',
+        url: 'localhost:3000/rest/order_confirmation',
+        body:{
+          cust_name: String,
+          order_number: Number,
+          price: Number,
+          payment_method: String,
+          product_name: String,
+          cust_address: String,
+          quantity: Number
+        }
+      },
+      response:{
+        statusCode: Number,
+        error: Object
+      }
+    },   
+    {
+      description: 'Get all order confirmation information', //US10
       request:{
         method: 'get',
         url: 'localhost:3000/rest/order_confirmation'
-        //params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          cust_name: Char,
-          order_number: Vachar,
-          price: Integer,
-          payment_method: char,
-          product_name: Char,
-          cust_address: Char,
-          quantity: Integer
+          cust_name: String,
+          order_number: Number,
+          price: Number,
+          payment_method: String,
+          product_name: String,
+          cust_address: String,
+          quantity: Number
         }
       ]
     }
   },    
   {
-    description: 'As a user I want to be able to upload a picture for my t-shirt', //US11
-      request:{
-        method: 'get',
-        url: 'localhost:3000/rest/product/5'
-        //params: ['id']
-      },
-      response:{
-        content:[
-          {
-          id: Number,
-          size: Char,
-          color: Char,
-          material: Char,
-          model: char,
-          image: jpg,
-          product_name: Char
-          }
-      ]
-    }
-  },
-  {
-    description: 'As a user I want to be able to use a discount code', //US24
-      request:{
-        method: 'get',
-        url: 'localhost:3000/rest/order/4'
-        //params: ['id']
-      },
-      response:{
-        content:[
-          {
-          id: Number,
-          order_number: Vachar,
-          price: Integer,
-          amount: Interger,
-          dis_code: Vachar
-          }
-      ]
-    }
-  },
-  {
-    description: 'As a user I want to be able to choose the amount of t-shirts', //US12
-      request:{
-        method: 'get',
-        url: 'localhost:3000/rest/order/3'
-        //params: ['id']
-      },
-      response:{
-        content:[
-          {
-          id: Number,
-          order_number: Vachar,
-          price: Integer,
-          amount: Interger,
-          dis_code: Vachar
-          }
-      ]
-    }
-  },
-  {
-    description: 'As a user I want to be able to change font size of the text on my t-shirt', //US13
+    description: 'Add product image',  //US11
     request:{
-      method: 'get',
-      url: 'localhost:3000/rest/customize/3'
-      //params: ['id']
+      method: 'post',
+      url: 'localhost:3000/rest/product',
+      body:{
+        image: String
+      }
     },
     response:{
-      content:{
-        id: Number,
-        font_theme: Char,
-        font_color: Char,
-        font_size: Char,
-        font_style: Char
-      }
+      statusCode: Number,
+      error: Object
     }
   },
   {
-    description: 'As a user I want to be able to change the text color', //US14
+    description: 'Get a product image', //US11
+      request:{
+        method: 'get',
+        url: 'localhost:3000/rest/product/5',
+        params: ['id']
+      },
+      response:{
+        content:
+          {
+          id: Number,
+          image: String
+          }
+    }
+  },
+  {
+    description: 'Get the discount codes', //US24
+      request:{
+        method: 'get',
+        url: 'localhost:3000/rest/order/4',
+        params: ['id']
+      },
+      response:{
+        content:[
+          {
+          id: Number,
+          dis_code: String
+          }
+      ]
+    }
+  },
+  {
+    description: 'Get the amount of t-shirts', //US12
+      request:{
+        method: 'get',
+        url: 'localhost:3000/rest/order/3',
+        params: ['id']
+      },
+      response:{
+        content:
+          {
+          id: Number,
+          amount: Number,
+          }
+    }
+  },
+  {
+    description: 'Get the font sizes', //US13
     request:{
       method: 'get',
-      url: 'localhost:3000/rest/customize/2'
-      //params: ['id']
+      url: 'localhost:3000/rest/customize/3',
+      params: ['id']
     },
     response:{
-      content:{
+      content:[
+      {
         id: Number,
-        font_theme: Char,
-        font_color: Char,
-        font_size: Char,
-        font_style: Char
+        font_size: Number,
       }
+    ]
     }
   },
   {
-    description: 'As a user I want to be able to choose any model from the t-shirt selections', //US20
+    description: 'Get the font colors', //US14
+    request:{
+      method: 'get',
+      url: 'localhost:3000/rest/customize/2',
+      params: ['id']
+    },
+    response:{
+      content:[
+      {
+        id: Number,
+        font_color: String
+      }
+    ]
+    }
+  },
+  {
+    description: 'Get t-shirt model', //US20
       request:{
         method: 'get',
-        url: 'localhost:3000/rest/product/4'
-        //params: ['id']
+        url: 'localhost:3000/rest/product/4',
+        params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          size: Char,
-          color: Char,
-          material: Char,
-          model: char,
-          image: jpg,
-          product_name: Char
+          model: String
           }
       ]
     }
   },
   {
-    description: 'As a user I want to be able to do invoice payment', //US21
+    description: 'Get the invoice payment', //US21
       request:{
         method: 'get',
-        url: 'localhost:3000/rest/payment_method/2'
-        //params: ['id']
+        url: 'localhost:3000/rest/payment_method/2',
+        params: ['id']
+      },
+      response:{
+        content:{
+          id: Number,
+          invoice: String
+          }
+    }
+  },
+  {
+    description: 'Get the card payment', //US22
+      request:{
+        method: 'get',
+        url: 'localhost:3000/rest/payment_method/1',
+        params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          card: Char,
-          invoice: Char,
-          direct: Char,
-          swish: char,
-          present_card: Char,
-          paypal: Char
+          card: String
           }
       ]
     }
   },
   {
-    description: ' As a user I want to be able to pay with card', //US22
-      request:{
-        method: 'get',
-        url: 'localhost:3000/rest/payment_method/1'
-        //params: ['id']
-      },
-      response:{
-        content:[
-          {
-          id: Number,
-          card: Char,
-          invoice: Char,
-          direct: Char,
-          swish: char,
-          present_card: Char,
-          paypal: Char
-          }
-      ]
-    }
-  },
-  {
-    description: 'As a user I want to be able to choose any payment options', //US23
+    description: 'Get all the payment options', //US23
       request:{
         method: 'get',
         url: 'localhost:3000/rest/payment_method'
-        //params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          card: Char,
-          invoice: Char,
-          direct: Char,
-          swish: char,
-          present_card: Char,
-          paypal: Char
+          card: String,
+          invoice: String,
+          direct: String,
+          swish: String,
+          present_card: String,
+          paypal: String
           }
       ]
     }
   },
   {
-    description: 'As a user I want to be able to choose a material of my t-shirt', //US25
+    description: 'Get t-shirt materials', //US25
       request:{
         method: 'get',
-        url: 'localhost:3000/rest/product/3'
-        //params: ['id']
+        url: 'localhost:3000/rest/product/3',
+        params: ['id']
       },
       response:{
         content:[
           {
           id: Number,
-          size: Char,
-          color: Char,
-          material: Char,
-          model: char,
-          image: jpg,
-          product_name: Char
+          material: String
           }
       ]
     }
